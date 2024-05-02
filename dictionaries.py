1c69d1b8a7a879d21ab6738d0dc801ba83e9b698
Artu <artuon@gmail.com>
2024-05-02 21:32:30 +0200

    Sets

--- dictionaries.py
+++ dictionaries.py
@@ -9,3 +9,9 @@ pet['age_years'] = 2
 print(pet)
 print(pet.values())
 print(pet.keys())
+
+
+set1 = {1, 2, 3, 4, 5}
+set2 = set([4, 5, 6, 7, 8])
+print({"set1": type(set1), "set2": type(set2)})
+print(set1.union(set2))
